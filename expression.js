c4a335e5b5c0564d4cc62fadbf00cadf5f76041f
vincenttommi <vincenttommi@gmail.com>
2023-03-21 18:55:37 +0300

    function expressions

---
+++ expression.js
@@ -0,0 +1,14 @@
+
+
+let sayHi = function(){
+    //function expressions  is a keyword used to define a function inside  an expression
+
+    alert("Hello");
+}
+
+alert(sayHi);
+
+// Function Expression
+let sum = function(a, b) {
+    return a + b;
+  };
