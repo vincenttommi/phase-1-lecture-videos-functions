1f3a18317408ce9f3d87b2e635d122dcab321058
vincenttommi <vincenttommi@gmail.com>
2023-03-21 18:35:44 +0300

    parameters in functions

---
+++ parameter.js
@@ -0,0 +1,11 @@
+//We can pass arbitrary data to functions using parameters.
+
+function showMessage(from, text){
+
+alert(from + ':'+ text);
+
+}
+
+ console.log(showMessage('vincent', 'mwangi!'));
+
+ 
